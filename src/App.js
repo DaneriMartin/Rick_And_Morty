e82ed145823a15b130efd5c7e8bed0dd0c28df77
DaneriMartin <daneri702@gmail.com>
2023-04-14 15:18:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards.jsx';
+import Nav from './components/Nav';
+import About from './components/About';
+import Detail from './components/Detail';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import Form from './components/Form';
+import { addFav, removeFav } from './redux/actions';
+import Favorites from './components/Favorites';
 
+const URL_BASE = "https://be-a-rym.up.railway.app/api/character"
+const API_KEY = "8fd3837b60b5.6609a595d2d5eea028ba";
+
+const email = "martin@gmail.com";
+const password = "123asd";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const location = useLocation();
+   const navigate = useNavigate();
+   const [characters, setCharacters] = useState([]);
+   const [access, setAccess] = useState(false);
+
+   const login = (userData) => {
+      if(userData.email === email && userData.password === password){
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/')
+   }, [access])
+
+   const onSearch = (id) => {
+      axios(`${URL_BASE}/${id}?key=${API_KEY}`)
+      .then(response => response.data)
+      .then((data) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   const onClose = (id) => {
+      const charactersFiltered = characters.filter(character => character.id !== id)
+      setCharacters(charactersFiltered)
+   }
+
+   return (
+      <div className='App'>
+         {
+            location.pathname !== '/' && <Nav onSearch={onSearch} setAccess={setAccess} />
+         }
+         
+         <Routes>
+            <Route path='/' element={<Form login={login}/>} />
+            <Route path='/home' element={ <Cards characters={characters} onClose={onClose}/> }/>
+            <Route path='/about' element={<About/>} />
+            <Route path='/detail/:id' element={<Detail/>} />
+            <Route path='/favorites' element={<Favorites/>} />
+         </Routes>
+        
+      </div>
+   );
 }
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+    return(
+        <div>
+            <h1>Proyecto de integracion M2 Henry</h1>
+            <p>practica para entender redux react </p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque cum dicta at vitae temporibus quod maiores quia libero corporis? Exercitationem nesciunt sapiente sunt illo tenetur fugiat ipsum rerum non minima.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique alias et porro necessitatibus at, eos, ut temporibus, nesciunt doloremque dolor eum quia reprehenderit nemo impedit cupiditate! Dolore beatae iusto sequi?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam officiis, 
+                assumenda magni nihil laboriosam perspiciatis, 
+                quibusdam optio vel voluptates deleniti quasi est ipsa vitae reiciendis
+                 possimus animi tempore aut aliquid!</p>
+        </div>
+
+    )
+}
+
+export default About;
